5c4df62e248316b70cee909af8944c91c7127462
AtharvaPatil1234 <meetatharvpatil@gmail.com>
2020-04-19 13:33:29 +0530

    updated UserLogin,UserLoginImpl

--- BusReservation/src/main/java/com/capgemini/service/UserLogin.java
+++ BusReservation/src/main/java/com/capgemini/service/UserLogin.java
@@ -1,13 +1,15 @@
 package com.capgemini.service;
 
+import com.capgemini.exception.InvalidUserPasswordException;
+import com.capgemini.exception.UserNotFoundException;
 import com.capgemini.model.User;
 
 public interface UserLogin {
 
-	//@@validateUserName
-		User validateUserName(String userName);
+		//@@validateUserName
+		User validateUserName(String userName) throws UserNotFoundException;
 		
 		//@@
 		//Validate passwaord
-		boolean passwordVerification(User user,String password);
+		boolean passwordVerification(User user,String password) throws InvalidUserPasswordException;
 }
--- BusReservation/src/main/java/com/capgemini/service/UserLoginImpl.java
+++ BusReservation/src/main/java/com/capgemini/service/UserLoginImpl.java
@@ -2,33 +2,29 @@ package com.capgemini.service;
 
 import com.capgemini.dao.UserDao;
 import com.capgemini.dao.UserDaoImpl;
+import com.capgemini.exception.InvalidUserPasswordException;
 import com.capgemini.exception.UserNotFoundException;
 import com.capgemini.model.User;
 
 public class UserLoginImpl implements UserLogin {
-	private static UserDao userDao = new UserDaoImpl();
-
-	@Override
-	public User validateUserName(String userName) {
-		
-		User user = null;
-		try {
-			user = userDao.getUserByUserName(userName);
-		} catch (UserNotFoundException e) {
-			System.out.println(e.getMessage());
-		}		
-		return user;
-	}
+		private static UserDao userDao = new UserDaoImpl();
 	
-	@Override
-	public boolean passwordVerification(User user, String password) {
 		
-		if(password.equals(user.getUserPassword())) {
-			return true;
-		}else {
-			//here you have to create InvalidUSerException in exception package and handle in this function
-			return false;
+		@Override
+		public User validateUserName(String userName) throws UserNotFoundException {	
+			User user = null;	
+			user = userDao.getUserByUserName(userName);		
+			return user;
 		}
-	}
+		
+		@Override
+		public boolean passwordVerification(User user, String password) throws InvalidUserPasswordException {
+			
+			if(password.equals(user.getUserPassword())) {
+				return true;
+			}else {
+				throw new InvalidUserPasswordException();
+			}
+		}
 
 }
