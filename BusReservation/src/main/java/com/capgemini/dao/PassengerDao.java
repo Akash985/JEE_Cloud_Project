fa1b5c472e9d0380f1068f0893d5137ee2db65d1
AtharvaPatil1234 <meetatharvpatil@gmail.com>
2020-04-19 12:58:53 +0530

    PassengerDao and PassengerDaoImpl upadated

--- BusReservation/src/main/java/com/capgemini/dao/PassengerDao.java
+++ BusReservation/src/main/java/com/capgemini/dao/PassengerDao.java
@@ -1,5 +1,9 @@
 package com.capgemini.dao;
 
+import com.capgemini.model.Passenger;
+
 public interface PassengerDao {
+	boolean addPassenger(Passenger pssgn, Long ticketNo);
+	boolean deletePassenger(Passenger pssgn, Long ticketNo);
 
 }
--- BusReservation/src/main/java/com/capgemini/dao/PassengerDaoImpl.java
+++ BusReservation/src/main/java/com/capgemini/dao/PassengerDaoImpl.java
@@ -1,18 +1,27 @@
 package com.capgemini.dao;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import com.capgemini.model.Passenger;
 
+public class PassengerDaoImpl implements PassengerDao{
+	
+	private static HashMap<Long, Passenger> passengerList = new HashMap<Long, Passenger>();
 
-public class PassengerDaoImpl {
+	@Override
+	public boolean addPassenger(Passenger pssgn, Long ticketNo) {
+		passengerList.put(ticketNo, pssgn);		
+		return true;
+	}
+
+	@Override
+	public boolean deletePassenger(Passenger pssgn, Long ticketNo) {
+		// TODO Auto-generated method stub
+		passengerList.remove(ticketNo, pssgn);
+		return true;
+	}
 	
-private static List<Passenger> passengerList = new ArrayList<Passenger>();
 	
-	
-	//add Passenger(i.e whichever passenger books ticket will be added to list)
-	//delete Passenger
 	//update Passenger information
 
 }
