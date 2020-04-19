370dc58a5d42492ed482610801b4f6e1a2e56102
Akash985 <akashshyamyadav1997@gmail.com>
2020-04-19 20:21:12 +0530

    Updated Book ticket of BookTicket.java and BookTicketImpl

--- BusReservation/src/main/java/com/capgemini/service/BookTicket.java
+++ BusReservation/src/main/java/com/capgemini/service/BookTicket.java
@@ -1,7 +1,7 @@
 package com.capgemini.service;
 
-
 import com.capgemini.exception.BookingFailedException;
+import com.capgemini.exception.SeatAlreadyOccupiedException;
 import com.capgemini.model.Bus;
 import com.capgemini.model.Passenger;
 
@@ -9,4 +9,3 @@ public interface BookTicket {
 	String bookTicket(String source,String destination, Bus bus, Passenger pssgn, int seatNumber, String journeyDate) throws BookingFailedException;
 	String showTicket(String source,String destination, Bus bus, Passenger pssgn, int seatNumber, String journeyDate);
 }
-
--- BusReservation/src/main/java/com/capgemini/service/BookTicketImpl.java
+++ BusReservation/src/main/java/com/capgemini/service/BookTicketImpl.java
@@ -1,6 +1,5 @@
 package com.capgemini.service;
 
-
 import com.capgemini.dao.FareDao;
 import com.capgemini.dao.FareDaoImpl;
 import com.capgemini.dao.PassengerDao;
@@ -8,6 +7,7 @@ import com.capgemini.dao.PassengerDaoImpl;
 import com.capgemini.dao.TicketDao;
 import com.capgemini.dao.TicketDaoImpl;
 import com.capgemini.exception.BookingFailedException;
+import com.capgemini.exception.SeatAlreadyOccupiedException;
 import com.capgemini.model.Bus;
 import com.capgemini.model.Passenger;
 import com.capgemini.model.Ticket;
@@ -15,18 +15,25 @@ import com.capgemini.model.Ticket;
 public class BookTicketImpl implements BookTicket {
 	private static TicketDao ticketDao = new TicketDaoImpl();
 	private static FareDao fareDao= new FareDaoImpl();
+	private static SeatArrangement seatarrgn = new SeatArrangementImpl();
 	private static PassengerDao pssgnDao = new PassengerDaoImpl();
+	
 	@Override
-	public String bookTicket(String source, String destination, Bus bus, Passenger pssgn, int seatNumber,String journeyDate) throws BookingFailedException {
-		
+	public String bookTicket(String source, String destination, Bus bus, Passenger pssgn, int seatNumber,String journeyDate) throws BookingFailedException{
+		boolean seatRes = false;
+		boolean ticketRes = false;
+		Ticket ticket = null;
 		//calculate fare
 		double fare = fareDao.calculateFare(source, destination, bus, pssgn);
-		Ticket ticket = new Ticket(pssgn.getPassengerName(), bus.getBusName(), bus.getBusNumber(), bus.getStartStop(), bus.getEndStop(), source, destination, fare,bus.getDepartTime(), journeyDate);
-		boolean res = ticketDao.storeTicketInDatabase(ticket);
-		if(res) {
+		seatRes =seatarrgn.bookSeat(bus, seatNumber, pssgn.getPassengerName());
+		if(seatRes) {
+		ticket = new Ticket(pssgn.getPassengerName(), bus.getBusName(), bus.getBusNumber(), bus.getStartStop(), bus.getEndStop(), source, destination, fare,bus.getDepartTime(), journeyDate);
+		ticketRes = ticketDao.storeTicketInDatabase(ticket);
+		}
+		if(ticketRes) {
 			pssgnDao.addPassenger(pssgn, ticket.getTicketNo());
 			return "Your ticket is confirmed and it as follows:\n"
-						+"Ticket No : "+ticket.getTicketNo()
+						+"Ticket No : "+ticket.getTicketNo()+"\n"
 						+showTicket(source, destination, bus, pssgn, seatNumber, journeyDate);
 		}
 		else {
@@ -37,14 +44,14 @@ public class BookTicketImpl implements BookTicket {
 
 	@Override
 	public String showTicket(String source, String destination, Bus bus, Passenger pssgn, int seatNumber, String journeyDate) {
-		
 		//calculate fare
 		double fare = fareDao.calculateFare(source, destination, bus, pssgn);
 		return "Bus Name : "+bus.getBusName()+"\n"
 					+"Bus no. :"+bus.getBusNumber()+"\n"
 					+"Bus starts from : "+bus.getStartStop()+"\n"
 					+"Bus final destination : "+bus.getEndStop()+"\n"
-					+"Departure time from Starting point : "+bus.getDepartTime()+" - "+journeyDate+"\n"
+					+"Date of Journey : "+journeyDate+"\n"
+					+"Departure time from Starting point : "+bus.getDepartTime()+"\n"
 					+"Your : \n"
 					+"Boarding point : "+source+"\n"
 					+"Dropping point : "+destination+"\n"
