9b3aa36b2b7cec614484fb80350d10d3cfb106f6
Akash985 <akashshyamyadav1997@gmail.com>
2020-04-19 13:38:09 +0530

    Updated App class in Ui Layer

--- BusReservation/src/main/java/com/capgemini/ui/App.java
+++ BusReservation/src/main/java/com/capgemini/ui/App.java
@@ -1,14 +1,27 @@
 package com.capgemini.ui;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import com.capgemini.dao.UserDao;
-import com.capgemini.dao.UserDaoImpl;
+import com.capgemini.exception.BookingFailedException;
+import com.capgemini.exception.BusNotAvailableException;
+import com.capgemini.exception.InvalidUserPasswordException;
+import com.capgemini.exception.PhoneNumberExistsException;
+import com.capgemini.exception.SeatNotAvailableException;
+import com.capgemini.exception.SourceAndDestinationAreEqualException;
+import com.capgemini.exception.UserNameExistsException;
+import com.capgemini.exception.UserNotFoundException;
 import com.capgemini.model.Bus;
 import com.capgemini.model.Passenger;
 import com.capgemini.model.User;
+import com.capgemini.service.BookTicket;
+import com.capgemini.service.BookTicketImpl;
 import com.capgemini.service.SearchBus;
 import com.capgemini.service.SearchBusImpl;
 import com.capgemini.service.SeatArrangement;
@@ -18,176 +31,279 @@ import com.capgemini.service.UserLoginImpl;
 import com.capgemini.service.UserSignUp;
 import com.capgemini.service.UserSignUpImpl;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		boolean flag =true;
+		boolean flag = true;
 		int age;
 		char gender;
-		
+
 		do {
-			System.out.println("*******************Bus Reservation*******************");
+			System.out.println("**********************Bus Reservation**********************");
 			System.out.println();
 			System.out.println("Welcome to Bus Reservation");
 			System.out.println();
 			System.out.println("1. Login");
 			System.out.println("2. SignUp");
 			System.out.println("3. Admin login");
-			System.out.println("4. Admin work for debuging");
 			System.out.println("0. Exit Application");
 			System.out.println("Enter your choice: ");
-			
-			int choice = input.nextInt();//$$$here also exception will come because if user enters input other than digit
-			while(choice!=1 && choice!=2 && choice!=3 && choice!=4 && choice!=0) {
-				System.out.println("Inavlid input!!!----enter either\'1\' or\'2\' or\'3\' or\'4\' or\'0\'");
-				choice = input.nextInt();
+
+			int choice = 0;
+			while (true) {
+				try {
+					choice = input.nextInt();
+
+					if (choice <= 0 || choice > 3) {
+						System.out.println("!!!You have to choose a choice from 0 to 3");
+						continue;
+					} else {
+						break;
+					}
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid input! You have to enter a choice");
+					input.next();
+					continue;
+				}
 			}
 			System.out.println();
-			
+
 			switch (choice) {
+
 			case 1:
 				User user;
 				boolean nextLoopflag;
-				String addword ="";
+				String addword = "";
 				UserLogin userLogin = new UserLoginImpl();
-				while(true) {
+				while (true) {
 					System.out.println("===========================================================================");
-					System.out.println("Enter your login Username "+addword+":");
+					System.out.println("Enter your login Username " + addword + ":");
 					String userName = input.next();
-					user = userLogin.validateUserName(userName);
-					if(user==null) {
-						System.out.println("!!!!!!!!!!invalid user name");//instead of this here we should handle inavlid username exception
-						System.out.println();
-						nextLoopflag = false;
-						addword ="again";
-						continue ;
-					}
-					else {
+					try {
+						user = userLogin.validateUserName(userName);
 						nextLoopflag = true;
 						System.out.println("Enter the password: ");
 						break;
-					}				
+					} catch (UserNotFoundException e) {
+						System.out.println(e.getMessage());
+						nextLoopflag = false;
+						addword = "again";
+						continue;
+					}
 				}
-				
-				while(nextLoopflag) {
+				while (nextLoopflag) {
 					String password = input.next();
-					boolean result = userLogin.passwordVerification(user, password);
-					if(result==false) {
+					boolean result;
+					try {
+						result = userLogin.passwordVerification(user, password);
+						nextLoopflag = false;
+						break;
+					} catch (InvalidUserPasswordException e) {
+						System.out.println(e.getMessage());
 						System.out.println("Enter the password again: ");
 						continue;
 					}
-					else {
-						nextLoopflag = false;
-						//flag=true;//used for debuging
-						break;
-					}
+
 				}
 				System.out.println("===========================================================================");
 				System.out.println("Go ahead!!!!");
-				
+
 				List<Bus> availBusList = new ArrayList<Bus>();
-				//ask for source to destination
-				System.out.println("Enter your Source");
-				String source = input.next();
-				System.out.println("Enter your destination");
-				String destination = input.next();
-				//display buses for that source to destination
-				SearchBus searchBus = new SearchBusImpl();
-				availBusList = searchBus.showAvailableBuses(source, destination);
-				System.out.println("Buses available for this route are as follows :");
-				for (int i = 0; i < availBusList.size(); i++) {
-					System.out.println((i+1)+". "+availBusList.get(i).getBusName());
+				String source;
+				String destination;
+				String dateOfTravel;
+				String day;
+				Bus selectedBus;
+				
+				
+				// ask for source to destination
+				label1: while (true) {
+					System.out.println("Enter your Source");
+					source = input.next();
+					System.out.println("Enter your destination");
+					destination = input.next();
+					System.out.println("Enter the date of travelin dd/m//yyyy");
+					dateOfTravel = input.next();
+					SearchBus searchBus = new SearchBusImpl();
+					try {
+						day = findDayOnThisDate(dateOfTravel);
+						availBusList = searchBus.showAvailableBuses(source, destination, day);
+					} catch (SourceAndDestinationAreEqualException e) {
+						System.out.println(e.getMessage());
+						continue label1;
+					} catch (BusNotAvailableException e) {
+						System.out.println(e.getMessage());
+						continue label1;
+					} catch (ParseException e) {
+						System.out.println("Date entered is not in a valid format!!!");
+						continue label1;
+					} 
+
+					System.out.println("Buses available for this route are as follows :");
+					for (int i = 0; i < availBusList.size(); i++) {
+						System.out.println((i + 1) + ". " + availBusList.get(i).getBusName() + " - "
+								+ availBusList.get(i).getDepartTime());
+					}
+					// here user selects the bus
+					System.out.println("Select the choice in which you want to travel from above list");
+					int busChoice = input.nextInt(); // ##here exception
+					selectedBus = availBusList.get(busChoice - 1);
+
+					
+					// show seat availability in that bus
+					label2: while (true) {
+						System.out.println("Seat available in " + selectedBus.getBusName() + " are as follows :");
+						SeatArrangement seatings = new SeatArrangementImpl();
+						try {
+							System.out.println(seatings.seatAvailability(selectedBus));
+						} catch (SeatNotAvailableException e) {
+							System.out.println(e.getMessage());
+							System.out.println("Try for other bus...");
+							continue label1;
+						}
+
+						// here user selects the seatNo
+						System.out.println("Enter the seat you want :");
+						int selectedSeatNo = input.nextInt();
+
+						// ask to enter passenger details
+						System.out.println("Enter the passenger details :");
+						System.out.println("Enter the passenger full name : ");
+						input.nextLine();
+						String pssgnName = input.nextLine();
+						System.out.println("Enter passenger gender :");
+						gender = input.next().charAt(0);
+						System.out.println("Enter passenger Age :");
+						age = input.nextInt();
+
+						Passenger pssgn = new Passenger(pssgnName, gender, age);
+
+						System.out.println("Your ticket details are:");
+						BookTicket bkticket = new BookTicketImpl();
+						System.out
+								.println(bkticket.showTicket(source, destination, selectedBus, pssgn, selectedSeatNo,dateOfTravel));
+
+						System.out.println("Do you want to confirm ticket (Y->yes/N->No)");
+						char choice2 = input.next().charAt(0);//############################################
+						if (choice2 == 'Y') {
+							try {
+								System.out.println(
+										bkticket.bookTicket(source, destination, selectedBus, pssgn, selectedSeatNo, dateOfTravel));
+							} catch (BookingFailedException e) {
+								System.out.println(e.getMessage());
+								System.out.println("Try again...");
+								continue label2;
+							}
+						} else {
+							System.out.println("you canceled to book");
+							continue label1;
+						}
+
+						System.out.println();
+						System.out.println("Do you want to book ticket again? \n Enter--> (Y->yes/N->No)");
+						char choice3 = input.next().charAt(0);//############################################
+						if (choice2 == 'Y') {
+							continue label2;
+						} else {
+							System.out.println("Do you want to goto home page or logout enter your choice ");
+							System.out.println("1. Home page \n 2. Logout");
+							int choice4;
+							
+							label3: while (true) {
+								try {
+									choice = input.nextInt();
+
+									if (choice < 0 || choice > 2) {
+										System.out.println("!!!You have to choose a choice either 1 or 2");
+										continue label3;
+									} else {
+										break label3;
+									}
+								} catch (InputMismatchException e) {
+									System.out.println("Invalid input! You have to enter a choice again");
+									input.next();
+									continue;
+								}
+							}
+							
+							if (choice == 1) {
+								continue label1;
+							} else {
+								break label1;
+							}
+
+						}
+					}
 				}
-				//here user selects the bus
-				System.out.println("Select the choice in which you want to travel from above list");
-				int busChoice = input.nextInt();				
-				Bus selectedBus = availBusList.get(busChoice-1);
-				
-				//show seat availability in that bus
-				System.out.println("Seat available in "+selectedBus.getBusName()+" are as follows :");
-				SeatArrangement seatings = new SeatArrangementImpl();
-				System.out.println(seatings.seatAvailability(selectedBus));
-				//here user selects the seatNo
-				System.out.println("Enter the seat you want :");
-				int selectedSeatNo = input.nextInt();
-				
-				//ask to enter passenger details
-				System.out.println("Enter the passenger details :");
-				System.out.println("Enter the passenger full name : ");
-				input.nextLine();
-				String pssgnName = input.nextLine();
-				System.out.println("Enter passenger gender :");
-				gender = input.next().charAt(0);
-				System.out.println("Enter passenger Age :");
-				age = input.nextInt();
-				
-				Passenger pssgn = new Passenger(pssgnName, gender, age);
-				
-				
-				
-				//Show the tickect for the above things,
-				//for eg we will call function-->String showTicket(Bus bus, Passenger pssgn, int seatNumber )
-				//then ask to confirm tickect
-				//after this call function --> String bookTicket(Bus bus, Passenger pssgn, int seatNumber)
-				
-				//ask user if he wants to add one more passenger 
-				
-				//do not forget to close all resurces
+				//############################################ do not forget to close all resurces
 				break;
+
 				
 			case 2:
 				System.out.println("===========================================================================");
 				System.out.println("Enter your Full Name:");
-				input.nextLine(); // This line you have to add (It consumes the \n character which was not read by nextInt() earlier)
-				String userFullName = input.nextLine();				
+				input.nextLine(); // This line you have to add (It consumes the \n character which was not read by
+									// nextInt() earlier)
+				String userFullName = input.nextLine();
 				System.out.println("Enter username you want:");
 				String userName = input.next();
 				System.out.println("Enter your age:");
 				age = input.nextInt();
 				System.out.println("Enter your Gender M/F:");
 				gender = input.next().charAt(0);
-				while(gender!='M'&&gender!='F') {
+				while (gender != 'M' && gender != 'F') {
 					System.out.println("!!!Enter either \"M\"->for male or \"F\"->for Female!!!");
 					gender = input.next().charAt(0);
 				}
 				System.out.println("Enter Your Mobile Number");
 				long phoneNumber = input.nextLong();
-				//create exception to validate phone number
+				//############################################## create exception to validate phone number
 				System.out.println("Set your password:");
 				String userPassword = input.next();
-				User newUser  = new User(userFullName, userName, userPassword, gender, age, phoneNumber);				
-				UserSignUp newUserSignUp = new  UserSignUpImpl();
-				boolean userCreated = newUserSignUp.signUp(newUser);
-				System.out.println("User created or not: "+userCreated);
-				System.out.println();
-				if(userCreated) {
-					//diplays deatils of new user creteds
+				User newUser = new User(userFullName, userName, userPassword, gender, age, phoneNumber);
+				UserSignUp newUserSignUp = new UserSignUpImpl();
+				boolean userCreated;
+				try {
+					userCreated = newUserSignUp.signUp(newUser);
+					System.out.println("User created or not: " + userCreated);
 					System.out.println();
+					if (userCreated) {
+						newUser.toString();
+						System.out.println();
+					}
+				} catch (UserNameExistsException | PhoneNumberExistsException e) {
+					System.out.println(e.getMessage());
 				}
+				
 			
+
 				break;
+
 				
 			case 3:
-				//admin login and services that admin can do
+				// admin login and services that admin can do
 				break;
-				
-			case 4:
 
-				break;
-				
-			case 0:
-				//this to exit from application
-				flag = false;
-				break;			
-
-			}			
 			
+			case 0:
+				// this to exit from application
+				flag = false;
+				break;
+
+			}
+
 		} while (flag);
-    }
+	}
+
+	static String findDayOnThisDate(String date) throws ParseException {
+		SimpleDateFormat SDFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date userDt;
+		String str = null;
+		userDt = SDFormat.parse(date);
+		SimpleDateFormat sdf = new SimpleDateFormat("E");
+		str = sdf.format(userDt);
+
+		return str;
+	}
+
 }
