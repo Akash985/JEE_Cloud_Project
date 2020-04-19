34560c623da18f413394c798d39f537426f596e9
Akash985 <akashshyamyadav1997@gmail.com>
2020-04-19 13:26:38 +0530

    Updated SeatArrange,SeatArrangeImpl,created SeatNotAvailableException

--- BusReservation/src/main/java/com/capgemini/service/SeatArrangement.java
+++ BusReservation/src/main/java/com/capgemini/service/SeatArrangement.java
@@ -3,28 +3,7 @@ package com.capgemini.service;
 import com.capgemini.exception.SeatNotAvailableException;
 import com.capgemini.model.Bus;
 
-public class SeatArrangementImpl implements SeatArrangement{
-
-	@Override
-	public StringBuffer seatAvailability(Bus bus) throws SeatNotAvailableException {
-		int count=0;
-		StringBuffer seats = new StringBuffer();
-		String tempBus[] = bus.getSeatArrangement();
-		for (int i = 0; i < tempBus.length; i++) {
-			if (tempBus[i] == null) {
-				count++;
-				seats.append((i+1)+"  ");
-				if (((i+1)%10)== 0) {
-					seats.append("\n");
-				}
-				
-			}
-		}
-		
-		if(count==0) {
-			throw new SeatNotAvailableException(bus.getBusName());
-		}
-		return seats;
-	}
+public interface SeatArrangement {
+	StringBuffer seatAvailability(Bus bus) throws SeatNotAvailableException ;
 
 }
