90d8c920c9d7ec676cf73745b028fdbbaca2216c
Akash985 <akashshyamyadav1997@gmail.com>
2020-04-20 19:40:00 +0530

    UserLoginTesT case uploaded

--- BusReservation/src/test/java/com/capgemini/service/UserLoginTest.java
+++ BusReservation/src/test/java/com/capgemini/service/UserLoginTest.java
@@ -1,14 +1,66 @@
  package com.capgemini.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.capgemini.exception.InvalidUserPasswordException;
+import com.capgemini.exception.UserNotFoundException;
+import com.capgemini.model.User;
+
 public class UserLoginTest {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValidateUserNameForUserNameExist() {
+		User user1 = new User("Atharva", "AtharvaP1997", "22222222", 'M', 21, 7856124389L);
+		UserLogin userLogin= new UserLoginImpl();
+		User user2 = null;
+		try {
+			user2 = userLogin.validateUserName(user1.getUserName());
+			user1.equals(user2);
+		} catch (UserNotFoundException e) {
+			e.getMessage();
+		}
+		 
 	}
-
+	
+	@Test(expected = UserNotFoundException.class)
+	public void testValidateUserNameUserNotFoundException() throws UserNotFoundException {
+		UserLogin userLogin= new UserLoginImpl();
+		User user2 = userLogin.validateUserName("Aaa123");		
+	}
+	
+	@Test
+	public void testPasswordVerificationPassed() {
+		User user1 = new User("Atharva", "AtharvaP1997", "22222222", 'M', 21, 7856124389L);
+		UserLogin userLogin= new UserLoginImpl();
+		try {
+			assertTrue(userLogin.passwordVerification(user1, "22222222"));
+		} catch (InvalidUserPasswordException e) {
+			System.out.println(e.getMessage());
+		}		
+	}
+	
+	@Test
+	public void testPasswordVerificationFailed() {
+		User user1 = new User("Atharva", "AtharvaP1997", "22222222", 'M', 21, 7856124389L);
+		UserLogin userLogin= new UserLoginImpl();
+		try {
+			assertFalse(userLogin.passwordVerification(user1, "11111111"));
+		} catch (InvalidUserPasswordException e) {
+			System.out.println(e.getMessage());
+		}		
+	}
+	
+	
+	@Test(expected = InvalidUserPasswordException.class)
+	public void testPasswordVerificationInvalidUserPassworException() throws InvalidUserPasswordException {
+		User user1 = new User("Atharva", "AtharvaP1997", "22222222", 'M', 21, 7856124389L);
+		UserLogin userLogin= new UserLoginImpl();
+		assertFalse(userLogin.passwordVerification(user1, "11111111"));
+	}
+	
+	
 }
